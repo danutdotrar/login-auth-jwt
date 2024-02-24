36652aa78bb1bb5471ae19e04592b04c1bfab4e6
danutdotrar <morarasu.danut@gmail.com>
2024-02-24 17:03:43 +0200

    Start baseQuery

--- redux-auth-jwt/src/app/api/apiSlice.js
+++ redux-auth-jwt/src/app/api/apiSlice.js
@@ -2,3 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
 // fetch the url and set headers token
+const baseQuery = fetchBaseQuery({
+    baseUrl: "http://localhost:3500",
+    // send back our http only secure cookie
+    // the cookie is send with every query
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {},
+});
